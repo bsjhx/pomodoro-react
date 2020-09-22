6e811a418f5a4653cd3b73b44e8392fcb0f20586
mrakoczy <rakoczymichal@hotmail.com>
2020-09-22 23:45:12 +0200

    Added timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [timer, setTimer] = useState(25 * 60);
+    let [isRunning, setRunning] = useState(false);
+
+    useInterval(() => {
+        setTimer(timer - 1);
+    }, isRunning ? 1000 : null);
+
+    const startCounting = () => {
+        setRunning(!isRunning);
+    };
+
+    return (
+        <div>
+            <div>
+                {timer}
+            </div>
+            <button onClick={startCounting}>START</button>
+            <button>RESET</button>
+        </div>
+    );
+}
+
+function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest function.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
 }
 
 export default App;
