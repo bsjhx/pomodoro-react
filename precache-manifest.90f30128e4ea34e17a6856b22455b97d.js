d4557aa930bf261a51f0e817e7dcff3d62726ad3
mrakoczy <rakoczymichal@hotmail.com>
2020-10-07 00:15:28 +0200

    Updates

---
+++ precache-manifest.90f30128e4ea34e17a6856b22455b97d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bdd28c4a74716b8d6b8cdd068f9659",
+    "url": "/pomodoro-react/index.html"
+  },
+  {
+    "revision": "b172d9bd121dca0639a7",
+    "url": "/pomodoro-react/static/css/main.77b27185.chunk.css"
+  },
+  {
+    "revision": "fb9ea6f4c0f82311a997",
+    "url": "/pomodoro-react/static/js/2.fc6c613d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-react/static/js/2.fc6c613d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b172d9bd121dca0639a7",
+    "url": "/pomodoro-react/static/js/main.0914e076.chunk.js"
+  },
+  {
+    "revision": "e46251b4a667c98edcc0",
+    "url": "/pomodoro-react/static/js/runtime-main.353efe95.js"
+  }
+]);
--- src/model/Phase.js
+++
@@ -1,8 +0,0 @@
-export class Phase {
-    constructor(orderId, name, seconds, next) {
-        this.orderId = orderId;
-        this.name = name;
-        this.seconds = seconds;
-        this.next = next;
-    }
-}
